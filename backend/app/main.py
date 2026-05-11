1b271fcd6ec6ed2e2b541d2aee6cb94ae1f51335
nikhil <nikhilkumarnk@nikhils-Air.Dlink>
2026-05-12 02:40:02 +0530

    cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,7 +27,7 @@ app = FastAPI(title=settings.PROJECT_NAME, version="1.0.0", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000","https://analytics-9smf.onrender.com","https://analytics-lyart-seven.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

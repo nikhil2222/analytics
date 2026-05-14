3207ab64592817223dcb978eab0ab9d597119e04
nikhil <nikhilkumarnk@nikhils-MacBook-Air.local>
2026-05-14 20:25:07 +0530

    updated config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -8,7 +8,10 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
     PROJECT_NAME: str = "Analytics Platform"
-
+    smtp_host: str
+    smtp_port: int
+    smtp_user: str
+    smtp_pass: str
     class Config:
         env_file = ".env"
 
